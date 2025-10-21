a94d7671b58dd6b57fc52192ffbe009b1a4624db
toryzb <tory.zb@gmail.com>
2025-10-21 18:40:08 +0300

    feat: solve third task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -21,9 +21,21 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList( l, k ) {
+  while (l !== null && l.value === k) {
+    l = l.next;
+  }
+  let current = l;
+
+  while (current !== null && current.next !== null) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
